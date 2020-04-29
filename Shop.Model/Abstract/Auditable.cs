32cd482a3f7f2be42bd561c572aeea0c561ed1bc
SunHalo <SunHalo@DESKTOP-FB3KT70>
2020-04-29 17:04:08 +0700

    Add Table Product

---
+++ Shop.Model/Abstract/Auditable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shop.Model.Abstract
+{
+    public abstract class Auditable :IAuditable
+    {
+        public DateTime? CreatedDate { set; get; }
+        [MaxLength(256)]
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedDate { set; get; }
+        [MaxLength(256)]
+        public string UpdatedBy { set; get; }
+        [MaxLength(256)]
+        public string MetaKeyWord { set; get; }
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Shop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Shop.Model.Abstract
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedDate { set; get; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedDate { set; get; }
+        string UpdatedBy { set; get; }
+
+        string MetaKeyWord { set; get; }
+        string MetaDescription { get; set; }
+
+        bool Status { get; set; }
+    }
+}
